2f7255bbfe76964699a3c0738cc3ea2648273f5e
Jordan Sexton <jordan@jordansexton.com>
2021-10-31 23:19:56 -0500

    use shx

--- build-test-examples.sh
+++ build-test-examples.sh
@@ -1,7 +1,5 @@
-examples=$(find test/examples -maxdepth 1 -mindepth 1 -type d)
-
-for example in $examples; do\
-    rm -rf ${example}/build; \
-    mkdir -p ${example}/build; \
+for example in $(shx ls -d test/examples/*/); do\
+    shx rm -rf ${example}/build; \
+    shx mkdir -p ${example}/build; \
     docker run --rm -it -v $(PWD)/${example}:/example --entrypoint /bin/bash ghcr.io/hyperledger-labs/solang -c "solang /example/contracts/*.sol -o /example/build --target solana -v"; \
 done
